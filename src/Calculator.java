7ba573d5d605f35fa5e7245a4a4eee41d7d8ce72
Nihal A <nihalavulan1@gmail.com>
2021-05-08 21:01:50 +0530

    Full design ready

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,14 +13,14 @@ public class Calculator {
 		//Frame:
 		JFrame jf=new JFrame("Calculator");
 		jf.setLayout(null);
-		jf.setSize(600,500);
+		jf.setSize(440,580);
 		jf.setVisible(true);
 		jf.setLocation(300,150);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//Label:
 		JLabel displayLabel=new JLabel("ON  ");
-		displayLabel.setBounds(30, 50, 540, 50);
+		displayLabel.setBounds(30, 30, 380, 60);
 		displayLabel.setBackground(Color.LIGHT_GRAY);
 		displayLabel.setOpaque(true);
 		displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -61,7 +61,7 @@ public class Calculator {
 				sixButton.setBounds(230,230,80,80);
 				sixButton.setFont(new Font("Montserrat", Font.PLAIN, 24));
 				jf.add(sixButton);
-		//Row 2
+		//Row 3
 				JButton oneButton=new JButton("1");
 				oneButton.setBounds(30,340,80,80);
 				oneButton.setFont(new Font("Montserrat", Font.PLAIN, 24));
@@ -77,11 +77,41 @@ public class Calculator {
 				threeButton.setFont(new Font("Montserrat", Font.PLAIN, 24));
 				jf.add(threeButton);
 				
+		//Row 4
+				JButton dotButton=new JButton(".");
+				dotButton.setBounds(30,450,80,80);
+				dotButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(dotButton);
 				
+				JButton zeroButton=new JButton("0");
+				zeroButton.setBounds(130,450,80,80);
+				zeroButton.setFont(new Font("Montserrat", Font.PLAIN, 24));
+				jf.add(zeroButton);
 				
+				JButton equalButton=new JButton("=");
+				equalButton.setBounds(230,450,80,80);
+				equalButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(equalButton);
+		//Operators
+				JButton divisionButton=new JButton("/");
+				divisionButton.setBounds(330,120,80,80);
+				divisionButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(divisionButton);
+				
+				JButton multiplyButton=new JButton("x");
+				multiplyButton.setBounds(330,230,80,80);
+				multiplyButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(multiplyButton);
+				
+				JButton subtractButton=new JButton("-");
+				subtractButton.setBounds(330,340,80,80);
+				subtractButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(subtractButton);
 		
-		
-		
+				JButton additonButton=new JButton("+");
+				additonButton.setBounds(330,450,80,80);
+				additonButton.setFont(new Font("Montserrat", Font.BOLD, 24));
+				jf.add(additonButton);
 
 		
 		
